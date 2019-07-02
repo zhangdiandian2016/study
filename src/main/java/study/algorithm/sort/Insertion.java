b0d897e64c62da6607b58b0432c7e18b0ee21b59
denny.zhang <787292659@qq.com>
2019-07-02 11:06:17 +0800

    add

---
+++ src/main/java/study/algorithm/sort/Insertion.java
@@ -0,0 +1,28 @@
+package study.algorithm.sort;
+
+/**
+ * @author denny.zhang
+ * @ClassName:Insertion
+ * @Description:插入排序：对于数组长度为N的数组<br>就像扑克牌插纸牌一样 <ul> <li>比较次数：N-1~N²/2  平均N²/4 </li> <li>交换次数：0~N²/2 平均N²/4</li> </ul>
+ * @date 2018年2月5日上午11:13:26
+ */
+public class Insertion extends MySort {
+
+    @SuppressWarnings("rawtypes")
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        //遍历n-1次，a[i]插入前i个数
+        for (int i = 1; i < n; i++) {
+            //System.out.println("i="+i);
+            //i之前的每两项比较，出现逆序，立即交换
+            for (int j = i; j > 0 && less(a[j], a[j - 1]); j--) {
+                exch(a, j, j - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Insertion.sort(a);
+        show(a);
+    }
+}
---
+++ src/main/java/study/algorithm/sort/Selection.java
@@ -0,0 +1,32 @@
+package study.algorithm.sort;
+
+/**
+ * @author denny.zhang
+ * @ClassName:Selection
+ * @Description:选择排序：对于数组长度为N的数组<br> <ul> <li>比较次数：(n-1)+(n-2)+...2+1=n(n-1)/2,大约N²/2次</li> <li>交换次数：N次</li> </ul>
+ * @date 2018年2月5日上午11:13:26
+ */
+public class Selection extends MySort {
+
+    public static void sort(Comparable[] a) {
+        int n = a.length;
+        //遍历n次，每次确定一个最小值
+        for (int i = 0; i < n; i++) {
+            //初始化最小值下标为i
+            int min = i;
+            //把i之后的每一项都和a[min]比较，求最小项
+            for (int j = i + 1; j < n; j++) {
+                if (less(a[j], a[min])) {
+                    min = j;
+                }
+            }
+            //a[i]和a[min]交换，第i位排序完毕
+            exch(a, i, min);
+        }
+    }
+
+    public static void main(String[] args) {
+        Selection.sort(a);
+        show(a);
+    }
+}
