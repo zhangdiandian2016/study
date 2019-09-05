439882d08c4021c1d96f39b5e42508019a06331a
denny.zhang <787292659@qq.com>
2019-09-05 18:15:58 +0800

    add

---
+++ src/main/java/study/algorithm/interview/BigNumberSum.java
@@ -0,0 +1,57 @@
+package study.algorithm.interview;
+
+/**
+ * 大整数相加求和：可优化点：int -21-21亿，9位数妥妥的计算。拆分大整数每9位数一个元素，分别求和。效率可极大提升。
+ *
+ * @author denny
+ * @date 2019/9/5 下午5:50
+ */
+public class BigNumberSum {
+    private static String bigNumberSum(String bigA, String bigB) {
+        // 1.把2个大整数用数组逆序存储，数组长度等于较大整数位数+1
+        int maxLength = bigA.length() > bigB.length() ? bigA.length() : bigB.length();
+        int[] arrayA = new int[maxLength + 1];
+        for (int i = 0; i < bigA.length(); i++) {
+            arrayA[i] = bigA.charAt(bigA.length() - 1 - i) - '0';
+        }
+        int[] arrayB = new int[maxLength + 1];
+        for (int i = 0; i < bigB.length(); i++) {
+            arrayB[i] = bigB.charAt(bigB.length() - 1 - i) - '0';
+        }
+        // 2. 构建result数组
+        int[] result = new int[maxLength + 1];
+
+        // 3. 遍历数组，按位相加
+        for (int i = 0; i < result.length; i++) {
+            int temp = result[i];
+            temp += arrayA[i];
+            temp += arrayB[i];
+            //是否进位
+            if (temp >= 10) {
+                temp = temp - 10;
+                result[i + 1] = 1;
+            }
+            result[i] = temp;
+        }
+
+        //4.转成数组
+        StringBuilder stringBuilder = new StringBuilder();
+        // 是否找到大整数的最高有效位
+        boolean findFirst = false;
+        for (int i = result.length - 1; i >= 0; i--) {
+            if (!findFirst) {
+                if (result[i] == 0) {
+                    continue;
+                }
+                findFirst = true;
+            }
+            stringBuilder.append(result[i]);
+        }
+        return stringBuilder.toString();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(bigNumberSum("4534647452342423", "986568568789664"));
+    }
+}
---
+++ src/main/java/study/algorithm/interview/removeKDigits.java
@@ -0,0 +1,49 @@
+package study.algorithm.interview;
+
+/**
+ * 删除整数的k个数字，使得留下的数字最小
+ *
+ * @author denny
+ * @date 2019/9/5 下午4:43
+ */
+public class removeKDigits {
+
+    /**
+     * 删除整数的k个数字，使得留下的数字最小
+     *
+     * @param num
+     * @param k
+     * @return
+     */
+    private static String removeKDigits(String num, int k) {
+        // 新长度
+        int newLength = num.length() - k;
+        // 创建栈，接收所有数字
+        char[] stack = new char[num.length()];
+        int top = 0;
+        // 遍历，一开始先入栈第一个数字。第一轮循环先给stack入栈一个数，且top++,往后循环，top-1才是栈顶
+        for (int i = 0; i < num.length(); ++i) {
+            // 当前数字
+            char c = num.charAt(i);
+            // 当栈顶数字 > 当前数字时，栈顶数字出栈，只要没删除够K个就一直往左边删除
+            while (top > 0 && stack[top - 1] > c && k > 0) {
+                // 这里top-1，就是出栈，忽略top
+                top -= 1;
+                k -= 1;
+            }
+            // 后一个数字入栈
+            stack[top++] = c;
+        }
+        // 找到栈中第一个非0数字的位置，以此构建新的字符串0000123->123
+        int offset = 0;
+        while (offset < newLength && stack[offset] == '0') {
+            offset++;
+        }
+        return offset == newLength ? "0" : new String(stack, offset, newLength - offset);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(removeKDigits("1593212", 3));
+        System.out.println(removeKDigits("10", 2));
+    }
+}
