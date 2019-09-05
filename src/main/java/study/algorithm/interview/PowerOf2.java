5fd33ad370562b7e1bb63e4f55044ba1c61e8ecc
denny.zhang <787292659@qq.com>
2019-09-05 11:38:14 +0800

    add

--- src/main/java/study/algorithm/interview/PowerOf2.java
+++ src/main/java/study/algorithm/interview/PowerOf2.java
@@ -10,15 +10,17 @@ public class PowerOf2 {
 
     /**
      * 判断是否是2的整数次幂： 2的整数次幂转换成二进制（1+n个0）& 二进制-1（n个1）=0
-     *
      * @param num
-     * @return
+     * @param a
+     * @return boolean
+     * @author denny
+     * @date 2019/9/5 上午11:14
      */
-    private static boolean isPowerOf2(int num) {
+    private static boolean isPowerOf2(int num, int a) {
         return (num & (num - 1)) == 0;
     }
 
     public static void main(String[] args) {
-        System.out.println("是否2的整数次幂=" + isPowerOf2(16));
+        System.out.println("是否2的整数次幂=" + isPowerOf2(16, 1));
     }
 }
---
+++ src/main/java/study/algorithm/interview/StackQueue.java
@@ -0,0 +1,67 @@
+package study.algorithm.interview;
+
+import java.util.Stack;
+
+/**
+ * 栈实现队列
+ *
+ * @author denny
+ * @date 2019/9/5 上午11:14
+ */
+public class StackQueue {
+    // 入队
+    private Stack<Integer> stackIn = new Stack<>();
+    // 出队
+    private Stack<Integer> stackOut = new Stack<>();
+
+    /**
+     * 入队：直接入栈
+     *
+     * @param element
+     */
+    private void enQueue(int element) {
+        stackIn.push(element);
+    }
+
+    /**
+     * 出队
+     *
+     * @return
+     */
+    private Integer deQueue() {
+        // 出队为空
+        if (stackOut.isEmpty()) {
+            // 如果入队为空，直接返回空
+            if (stackIn.isEmpty()) {
+                return null;
+            }
+            // 入队不为空，IN元素全部转移到OUT
+            transfer();
+        }
+        // 出队不为空，直接弹出
+        return stackOut.pop();
+    }
+
+    /**
+     * 入队元素转到出队
+     */
+    private void transfer() {
+        while (!stackIn.isEmpty()) {
+            stackOut.push(stackIn.pop());
+        }
+    }
+
+    public static void main(String[] args) {
+        StackQueue stackQueue = new StackQueue();
+        stackQueue.enQueue(1);
+        stackQueue.enQueue(2);
+        stackQueue.enQueue(3);
+        System.out.println("出队：" + stackQueue.deQueue());
+        System.out.println("出队：" + stackQueue.deQueue());
+        stackQueue.enQueue(4);
+        System.out.println("出队：" + stackQueue.deQueue());
+        System.out.println("出队：" + stackQueue.deQueue());
+
+    }
+
+}
